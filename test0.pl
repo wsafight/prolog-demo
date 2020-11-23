c1be5634c749fcad1f5203fc3b833c11c0590470
jump-and-jump <984292420@qq.com>
2020-11-23 23:56:47 +0800

    Create test0.pl

---
+++ test0.pl
@@ -0,0 +1,19 @@
+friend(john, julia).
+friend(john, jack).
+friend(julia, sam).
+friend(julia, molly).
+
+
+
+loves(john, julia).
+loves(julia, sam).
+loves(sam, julia).
+
+male(brad).
+male(john).
+male(jim).
+male(alfred).
+female(marry).
+child(brad, alfred).
+child(john, jim).
+child(john, marry).
